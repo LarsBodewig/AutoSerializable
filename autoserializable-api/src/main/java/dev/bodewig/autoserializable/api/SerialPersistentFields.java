19d056a750d6de80c713d3fe7e4f5a9d63751f49
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2025-09-16 13:22:38 +0200

    Added SerialPersistentFields annotation

---
+++ autoserializable-api/src/main/java/dev/bodewig/autoserializable/api/SerialPersistentFields.java
@@ -0,0 +1,15 @@
+package dev.bodewig.autoserializable.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SerialPersistentFields {
+
+    String FIELD_NAME = "serialPersistentFields";
+
+    String[] value();
+}
